9de46fe39173b51ca038e952b213cfb231d43da3
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2021-01-25 13:01:07 +0200

    working on the navigation section of the site when done will move to the search component section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import Avatar from '@material-ui/core/Avatar/Avatar';
+import LanguageIcon from '@material-ui/icons/Language'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+function Header() {
+    return (
+        <div className='header'>
+            <img className='header__logo' src={process.env.PUBLIC_URL + 'airbnblogo.png'} alt=""/>
+
+            <div className='header__center'>
+                <input type='text'/>
+                <SearchIcon/>
+            </div>
+
+            <div className='header__right' >
+                <p>Become a host</p>
+                <LanguageIcon/>
+                <ExpandMoreIcon/>
+                <Avatar/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import Banner from '../Banner/Banner'
+
+function Home() {
+    return (
+        <div className='home'>
+            <Banner/>
+        </div>
+    )
+}
+
+export default Home
